559af0068c48f4480a83d1e6c8c06313d5bccb1e
s18548 <s18548@pjwstk.edu.pl>
2020-03-09 16:30:51 +0100

    Praca_domowa_v0,01
    Baza do skończenia.
    Player się porusza.
    Ruchoma platforma działa.

---
+++ Praca_domowa_01/Assets/Moving_platform.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moving_platform : MonoBehaviour
+{
+    public float speed = 1;
+
+    private bool moveRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveRight = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x > 2)
+            moveRight = false;
+        if (transform.position.x < -1)
+            moveRight = true;
+
+        if (moveRight)
+            transform.position = new Vector2(transform.position.x + speed * Time.deltaTime, transform.position.y);
+        else
+            transform.position = new Vector2(transform.position.x - speed * Time.deltaTime, transform.position.y);
+    }
+}
